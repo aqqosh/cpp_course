ad47afb56f40fd20a2b190fef9e5d8c7da0b5bf3
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-10-23 21:32:10 +0300

    started arithmetic operations

---
+++ 05_arithmetic_operations.cpp
@@ -0,0 +1,75 @@
+/*
+When printf() is used, a specifier is needed in order to tell 
+the system what type of data you want to print. The %d tells 
+the system to print an integer and %f tells the system to print 
+a floating point number. If you use an incorrect specifier, you 
+will receive an error message. By default, floating point 
+numbers contain six zeros after the decimal point if they are 
+printed using printf().
+The \n in printf() is equivalent to endl. They both print a 
+newline character. Removing the \n from printf("%d \n", a); will 
+delete the newline character and cause the variables a and b to
+be printed side-by-side.
+
+Incrementing a variable means to increase the value of a 
+variable by a set amount. The most common incrementation you will 
+see is when a variable increments itself by the value of 1.
+
+String concatenation is the act of combining two strings together. 
+This is done with the + operator.
+
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  //add code below this line
+  // quick look into the operator +
+    cout << 7 + 3 << endl;
+  
+    double a = 7.1;
+    int b = +3;
+
+    cout << a + b << endl;
+
+    // look how cout print double
+    int c = 1;
+    double d = 1.0;
+    cout << c << endl;
+    cout << d << endl;
+
+    //  comparing cout and printf
+    int e = 1;
+    double f = 1.0;
+    cout << e << endl;
+    cout << f << endl;
+    printf("%d \n", e);
+    printf("%f \n", f);
+
+    // exploring incrementing
+    int g = 0;
+    int h = 0;
+    g = g + 1;
+    h++;
+    cout << g << endl;
+    cout << h << endl;
+
+    // string concatenations
+    string j = "This is an ";
+    string k = "example string";
+    string l = j + k;
+    cout << l << endl;
+
+    // substraction
+    int m = 10;
+    int n = 3;
+    int o = m - n;
+    cout << c << endl;
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
