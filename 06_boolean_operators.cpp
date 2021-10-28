7d8b14e30d2890452454e868a9834f293f082a07
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-10-28 21:09:53 +0300

    equality, less than, greater than

---
+++ 06_boolean_operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+	
+int main() {
+
+    // variables
+    int a = 5;
+    bool b = 7;
+
+    // equality
+    cout << boolalpha << (a == b) << endl;
+    cout << boolalpha << (a != b) << endl;
+
+    // less than
+    cout << boolalpha << (a < b) << endl;
+    cout << boolalpha << (a <= b) << endl;
+
+
+    // greater than
+    cout << boolalpha << (a > b) << endl;
+    cout << boolalpha << (a >= b) << endl;
+
+
+
+
+    return 0;
+
+}
