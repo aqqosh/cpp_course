29689b89281c5aff3b488f48113f2d22b6be93ce
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-10-28 21:32:58 +0300

    and, or

--- 06_boolean_operators.cpp
+++ 06_boolean_operators.cpp
@@ -20,8 +20,31 @@ int main() {
     cout << boolalpha << (a > b) << endl;
     cout << boolalpha << (a >= b) << endl;
 
+    // another variables
+    bool c = true;
+    bool d = true;
+    bool e = false;
 
+    // && and
+    cout << boolalpha << (c && d) << endl;
+    cout << boolalpha << (c && d && e) << endl;
 
+    // || or
+    bool f = true;
+    bool g = true;
+    bool h = false;
+    bool i = false;
+    cout << boolalpha << (f || g) << endl;
+    cout << boolalpha << (a || b || c) << endl;
+
+    // ! not
+    cout << boolalpha << (! true) << endl;
+
+    /*
+    If C++ can determine the result of a boolean expression 
+    before evaluating the entire thing, it will stop and 
+    return the value.
+    */
 
     return 0;
 
